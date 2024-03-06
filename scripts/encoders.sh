a98ff0926d080611ac240829b868d6c1e87a3a82
Stefan Hermann <stefan.a.hermann@gmail.com>
2024-03-06 10:11:32 +0100

    added most scripts, hostname

--- scripts/encoders.sh
+++ scripts/encoders.sh
@@ -8,8 +8,9 @@ rm -f $f
 
 (cd ../pthash/out && ./build -n 10000000 -l 5 -a 1 -e all -b opt2 -p 4882 -t 8 --dense --sort) &>> $f
 
-sed -i 's/": "/=/g' $0.res
-sed -i 's/"//g' $0.res
-sed -i 's/{/RESULT /g' $0.res
-sed -i 's/}//g' $0.res
-sed -i 's/,//g' $0.res
+
+sed -i 's/": "/=/g' $f
+sed -i 's/"//g' $f
+sed -i 's/{/RESULT /g' $f
+sed -i 's/}//g' $f
+sed -i 's/,//g' $f
