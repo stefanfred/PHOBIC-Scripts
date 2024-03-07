ef9f5cff407c544a1499b80eb4559e3ee46d1f8e
stefanfred <uqnhb@student.kit.edu>
2024-03-07 11:14:53 +0100

    131

--- scripts/gpucpuscaling.sh
+++ scripts/gpucpuscaling.sh
@@ -7,12 +7,12 @@ rm -f $f
 
 
 
-for i in `seq 13 26`
+for i in `seq 16 27`
 do
 	n=$(echo "2^$i" | bc)
 	p=$(echo "2^($i - 11)" | bc)
-	(cd ../pthash/out && ./build -n $n -l 8 -a 1 -e all -b opt2 -p $p -t 8 --dense --sort)  >> $f 2>&1
-	(cd ../PTHash-GPU/out/ && ./BENCHMARK -n $n -a 8 -e ortho -b r -i murmur2 -k direct)  >> $f 2>&1
+	(cd ../pthash/out && ./build -n $n -l 4 -a 1 -e all -b opt2 -p $p -t 8 --dense --sort)  >> $f 2>&1
+	(cd ../PTHash-GPU/out/ && ./BENCHMARK -n $n -a 4 -e ortho -b r -i murmur2 -k direct)  >> $f 2>&1
 
 done
 
