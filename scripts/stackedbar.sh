6ded481953cd4f862a2e61f2c32d95caaf63879f
Stefan Hermann <stefan.a.hermann@gmail.com>
2024-03-07 11:40:08 +0100

    stacked

---
+++ scripts/stackedbar.sh
@@ -0,0 +1,19 @@
+h=$(hostname)
+mkdir -p ${h}
+name=${0##*/}
+f=$h/"$name.res"
+printf "writing to %s\n" $f
+rm -f $f
+
+
+
+for i in `seq 2 12`
+do
+	(cd ../PTHash-GPU/out/ && ./BENCHMARK -n 25M -a $i -i murmur2)  >> $f 2>&1
+done
+
+sed -i 's/": "/=/g' $f
+sed -i 's/"//g' $f
+sed -i 's/{/RESULT /g' $f
+sed -i 's/}//g' $f
+sed -i 's/,//g' $f
