9200229d809aba7288302e85332f78ceb8bece7a
Stefan Hermann <stefan.a.hermann@gmail.com>
2024-04-12 10:41:14 +0200

    added 3rd to bucketsizes

--- scripts/bucketsizes.sh
+++ scripts/bucketsizes.sh
@@ -7,15 +7,19 @@ rm -f $f
 
 
 
-for i in `seq 5 11`
+for i in `seq 6 16`
 do
-	l=$(echo "$i" | bc)
-	(cd ../pthash/out && ./build -n 100000 -l $l -a 1 -e all -b opt -r add) >> $f 2>&1
-	(cd ../pthash/out && ./build -n 100000 -l $l -a 1 -e all -b skew -r add) >> $f 2>&1
-done
+	l=$(echo "$i / 2.0" | bc -l)
+	printf "lambda %s\n" $l	
+	(cd ../pthash/out && ./build -n 1000000 -l $l -a 1 -e all -b opt -r add --dense --sort -t 8 -q 0 -p 2500) >> $f 2>&1
+	(cd ../pthash/out && ./build -n 1000000 -l $l -a 1 -e all -b opt -r xor -q 0) >> $f 2>&1
+	(cd ../pthash/out && ./build -n 1000000 -l $l -a 1 -e all -b skew -r xor -q 0) >> $f 2>&1
+	
+	
 
-sed -i 's/": "/=/g' $f
-sed -i 's/"//g' $f
-sed -i 's/{/RESULT /g' $f
-sed -i 's/}//g' $f
-sed -i 's/,//g' $f
+	sed -i 's/": "/=/g' $f
+	sed -i 's/"//g' $f
+	sed -i 's/{/RESULT /g' $f
+	sed -i 's/}//g' $f
+	sed -i 's/,//g' $f
+done
