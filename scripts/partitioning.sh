a6be388e92b90ec6c5b7158a3a0aaed3641a4c60
stefanfred <uqnhb@student.kit.edu>
2024-04-17 19:57:21 +0200

    part plot new buckter

--- scripts/partitioning.sh
+++ scripts/partitioning.sh
@@ -7,11 +7,24 @@ rm -f $f
 
 
 
-for i in `seq 10 19`
+for i in `seq 10 12`
 do
 	p=$(echo "2 ^ $i + 2 ^ ($i-1)" | bc)
 	printf "index %s\n" $p
-	(cd ../pthash/out && ./build -n 100000000 -l 5 -a 1 -e all -b opt -p $p -t 8 --dense --sort -r xor -q 0) >> $f 2>&1
+	(cd ../pthash/out && ./build -n 100000000 -l 4 -a 1 -e all -b opt -p $p -t 8 --dense --sort -r add -q 0) >> $f 2>&1
+	
+	sed -i 's/": "/=/g' $f
+	sed -i 's/"//g' $f
+	sed -i 's/{/RESULT /g' $f
+	sed -i 's/}//g' $f
+	sed -i 's/,//g' $f
+done
+
+for i in `seq 13 19`
+do
+	p=$(echo "2 ^ $i + 2 ^ ($i-1)" | bc)
+	printf "index %s\n" $p
+	(cd ../pthash/out && ./build -n 100000000 -l 4 -a 1 -e all -b opt -p $p -t 8 --dense --sort -r xor -q 0) >> $f 2>&1
 	
 	sed -i 's/": "/=/g' $f
 	sed -i 's/"//g' $f
@@ -25,7 +38,7 @@ for i in `seq 20 23`
 do
 	p=$(echo "2 ^ $i + 2 ^ ($i-1)" | bc)
 	printf "index %s\n" $p
-	(cd ../pthash/out && ./build -n 100000000 -l 5 -a 1 -e mono-R -b opt -p $p -t 8 --dense --sort -r xor -q 0) >> $f 2>&1
+	(cd ../pthash/out && ./build -n 100000000 -l 4 -a 1 -e mono-R -b opt -p $p -t 8 --dense --sort -r xor -q 0) >> $f 2>&1
 	
 	sed -i 's/": "/=/g' $f
 	sed -i 's/"//g' $f
