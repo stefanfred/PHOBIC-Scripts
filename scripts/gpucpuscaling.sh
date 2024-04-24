6733afe23cb6d001ac6d49de9136346f91c81043
stefanfred <uqnhb@student.kit.edu>
2024-04-24 16:56:53 +0200

    refactored

--- scripts/gpucpuscaling.sh
+++ scripts/gpucpuscaling.sh
@@ -11,7 +11,7 @@ for i in `seq 14 27`
 do
 	n=$(echo "2^$i - 2^($i-2)" | bc)
 	(cd ../pthash/out && ./build -q 0 -n $n -l 8 -a 1 -e inter-R -b opt -p 2500 -t 8 -r add --dense --sort)  >> $f 2>&1
-	(cd ../PTHash-GPU/out/ && ./BENCHMARK -n $n -l 8 -e inter -b r -p 2500 -q 0)  >> $f 2>&1
+	(cd ../PHOBIC-GPU/out/ && ./BENCHMARK -n $n -l 8 -e inter -b r -p 2500 -q 0)  >> $f 2>&1
 
 done
 
--- scripts/stackedbar.sh
+++ scripts/stackedbar.sh
@@ -9,7 +9,7 @@ rm -f $f
 
 for i in `seq 2 12`
 do
-	(cd ../PTHash-GPU/out/ && ./BENCHMARK -n 100M -l $i -p 2500 -q 0)  >> $f 2>&1
+	(cd ../PHOBIC-GPU/out/ && ./BENCHMARK -n 100M -l $i -p 2500 -q 0)  >> $f 2>&1
 done
 
 sed -i 's/": "/=/g' $f
